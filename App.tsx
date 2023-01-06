2895d6dfb814f71cfa5615440f05b4a4cdece49a
Joanderson Silva <jhonny_040996@hotmail.com>
2023-01-06 16:36:10 -0300

    feat⭐: statusBar

--- App.tsx
+++ App.tsx
@@ -1,5 +1,10 @@
 import { ThemeProvider } from "styled-components/native";
-import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto'; 
+import { StatusBar } from "react-native";
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
 
 import theme from "./src/theme";
 import { Groups } from "@screens/Groups";
@@ -10,7 +15,12 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      {fontsLoaded ? <Groups/> : <Loading /> }
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      {fontsLoaded ? <Groups /> : <Loading />}
     </ThemeProvider>
   );
 }
