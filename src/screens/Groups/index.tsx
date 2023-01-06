6c25c1b0dadbc0bddf1b59024cfcd1e05bffabc2
Joanderson Silva <jhonny_040996@hotmail.com>
2023-01-06 16:49:44 -0300

    feat⭐: componente header

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,9 +1,10 @@
+import { Header } from '@components/Header';
 import { Container } from './styled';
 
 export function Groups() {
   return (
     <Container>
-    
+      <Header />
     </Container>
   );
 }
