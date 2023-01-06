fcd41568d116c2e7346571a02986d57c5c3cdd13
Joanderson Silva <jhonny_040996@hotmail.com>
2023-01-06 18:21:17 -0300

    feat⭐:  componente de card

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,18 +1,25 @@
-import { GroupCard } from '@components/GroupCard';
-import { Header } from '@components/Header';
-import { Highlight } from '@components/Highlight';
-import { Container } from './styled';
+import { GroupCard } from "@components/GroupCard";
+import { Header } from "@components/Header";
+import { Highlight } from "@components/Highlight";
+import { useState } from "react";
+import { FlatList } from "react-native";
+import { Container } from "./styled";
 
 export function Groups() {
+  const [groups, setGroups] = useState<string[]>([
+    "Galera da Rocket",
+    "amigos",
+  ]);
+
   return (
     <Container>
       <Header />
-      <Highlight 
-        title="Turmas"
-        subtitle="jogue com a sua turma"
+      <Highlight title="Turmas" subtitle="jogue com a sua turma" />
+      <FlatList
+        data={groups}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => <GroupCard title={item} />}
       />
-      <GroupCard 
-        title="Galera do Ignite" /> 
     </Container>
   );
 }
