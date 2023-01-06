89d99115215375e28aa2a12801bf5f7658cc2fc5
Joanderson Silva <jhonny_040996@hotmail.com>
2023-01-06 17:01:59 -0300

    feat⭐: adicionando componente voltar

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,20 @@
-import { Container, Logo } from "./styled";
+import { BackButton, BackIcon, Container, Logo } from "./styled";
 
-import logoImg from '@assets/logo.png';
+import logoImg from "@assets/logo.png";
 
-export function Header() {
+type Props = {
+  showBackButton?: boolean;
+};
+
+export function Header({ showBackButton = false }: Props) {
   return (
     <Container>
+      {showBackButton && (
+        <BackButton>
+          <BackIcon />
+        </BackButton>
+      )}
       <Logo source={logoImg} />
     </Container>
-  )
+  );
 }
