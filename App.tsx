8599d1fcf2c20e0b3b2f77f79c6e23f3c76ab7c7
Joanderson Silva <jhonny_040996@hotmail.com>
2023-01-06 16:27:46 -0300

    feat⭐: adiconando fontes

--- App.tsx
+++ App.tsx
@@ -1,12 +1,16 @@
 import { ThemeProvider } from "styled-components/native";
+import { ActivityIndicator } from 'react-native';
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto'; 
 
 import theme from "./src/theme";
 import { Groups } from "@screens/Groups";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
   return (
     <ThemeProvider theme={theme}>
-      <Groups />
+      {!fontsLoaded ? <Groups/> : <ActivityIndicator /> }
     </ThemeProvider>
   );
 }
--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -3,7 +3,7 @@ import { Container } from './styled';
 export function Groups() {
   return (
     <Container>
-        Groups
+    
     </Container>
   );
 }
