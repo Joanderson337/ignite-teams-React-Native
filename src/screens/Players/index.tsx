3fe72261b73dff02743e27280232adb89cbe6994
Joanderson Silva <jhonny_040996@hotmail.com>
2023-01-06 22:17:32 -0300

    feat⭐:  ButtonIcon custom

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,17 +1,26 @@
+import { ButtonIcon } from "@components/ButtonIcon";
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
+import { Input } from "@components/Input";
 
-import { Container } from "./styled";
+import { Container, Form } from "./styled";
 
 export function Players() {
   return (
     <Container>
       <Header showBackButton />
 
-      <Highlight 
+      <Highlight
         title="Nome da turma"
         subtitle="adicione a galera e separe os times"
       />
+
+      <Form>
+        <Input placeholder="Nome da pessoa" autoCorrect={false} />
+
+        <ButtonIcon icon="add" />
+      </Form>
+      
     </Container>
-  )
+  );
 }
